b7d9c1396fd651bb42c29e6bf362aaa77e075cd5
charlie-galb <galbraith.charlie@gmail.com>
2021-01-28 11:25:15 +0000

    Add middleware for cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require("cors")
+
+app.use(cors())
+app.use(express.json())
 
 app.listen(5000, () => {
-    console.log("Serv er has started on port 5000")
+    console.log("Server has started on port 5000")
 })
