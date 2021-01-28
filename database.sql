23131a538af31817717de136f008657837db5b58
charlie-galb <galbraith.charlie@gmail.com>
2021-01-28 11:25:47 +0000

    Add SQL commands for database creation and users table

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE chitter_dev;
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY, 
+    handle VARCHAR (50) UNIQUE,
+    password VARCHAR (50),
+    session_key VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+CREATE DATABASE chitter_test;
