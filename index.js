c19aff5361987ce1204846510620bc8c6e255b9a
charlie-galb <galbraith.charlie@gmail.com>
2021-02-01 12:29:50 +0000

    Remove middleware to createServer

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const express = require('express');
-const app = express();
-const cors = require("cors");
-usersRouter = require('./routers/usersRouter');
+const createServer = require("./createServer")
 
-
-app.use(cors());
-app.use(express.json());
-app.use('/users', usersRouter);
+const app = createServer()
 
 app.listen(5000, () => {
     console.log("Server has started on port 5000")
