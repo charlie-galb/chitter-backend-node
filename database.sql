e84ac7039999bc3e6790ded1c8362b4b505b6422
charlie-galb <galbraith.charlie@gmail.com>
2021-02-01 12:28:48 +0000

    Add SQL for test db

--- database.sql
+++ database.sql
@@ -8,6 +8,12 @@ CREATE TABLE users(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
-
 CREATE DATABASE chitter_test;
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY, 
+    handle VARCHAR (50) UNIQUE,
+    password VARCHAR (50),
+    session_key VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
