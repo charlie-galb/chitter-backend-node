8a7760d4454c146491521c6bccf17e88dbbb2439
charlie-galb <galbraith.charlie@gmail.com>
2021-02-01 12:29:15 +0000

    Add conditional for test and development databases

--- db.js
+++ db.js
@@ -2,10 +2,18 @@ require('dotenv').config()
 
 const Pool = require('pg').Pool
 
+let database;
+
+if(process.env.NODE_ENV === "development") {
+    database = process.env.DEV_DATABASE
+} else if (process.env.NODE_ENV === "test") {
+    database = process.env.TEST_DATABASE
+}
+
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
+    database: database,
     password: process.env.DB_USER_PASSWORD,
     port: process.env.DB_PORT,
 })
