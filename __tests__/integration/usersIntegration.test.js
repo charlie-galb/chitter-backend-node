59961b305cb22fc8e058d319724006659eb2e9c0
charlie-galb <galbraith.charlie@gmail.com>
2021-02-02 13:15:01 +0000

    Add tests for delete and get all users

--- __tests__/integration/usersIntegration.test.js
+++ __tests__/integration/usersIntegration.test.js
@@ -2,13 +2,13 @@ const createServer = require("../../createServer")
 const request = require('supertest');
 const pool = require("../../db");
 
-beforeEach( async () => {
+beforeAll( async () => {
 	return await pool.query(
         "CREATE TABLE users(user_id SERIAL PRIMARY KEY, handle VARCHAR (50) UNIQUE, password VARCHAR (50), session_key VARCHAR(50), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
         );
 })
 
-afterEach( async () => {
+afterAll( async () => {
 	return await pool.query("DROP TABLE users;");
 })
 
@@ -19,11 +19,29 @@ test("POST /users", async () => {
     await request(app)
     .post("/users")
     .send(userData)
-    .expect(200)
+    .expect(201)
     .then((response) => {
-        console.log(response.body)
-        // Check the response data
         expect(response.body.user_id).toBe(1)
         expect(response.body.handle).toBe("kay")
      })
-    })
+})
+
+test("GET /users", async () => {
+    await request(app)
+    .get("/users")
+    .expect(200)
+    .then((response) => {
+        expect(response.body.length).toBe(1)
+        expect(response.body[0].user_id).toBe(1)
+        expect(response.body[0].handle).toBe("kay")
+     })
+})
+
+test("DELETE /users", async () => {
+    await request(app)
+    .delete("/users/1")
+    .expect(200)
+    .then((response) => {
+        expect(response.text).toBe("User successfully deleted")
+     })
+})
