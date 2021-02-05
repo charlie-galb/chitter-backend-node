40ffdccf202541b9cdf46fc1a2ab02ea7e67ec72
charlie-galb <galbraith.charlie@gmail.com>
2021-02-05 14:10:20 +0000

    Add test for unhappy path: user handle and password blank

--- __tests__/controllers/usersController.test.js
+++ __tests__/controllers/usersController.test.js
@@ -5,13 +5,12 @@ const controller = require('../../controllers/usersController.js')
 jest.mock('../../queries/userQueries');
 
 
-afterAll(() => {
-	jest.unmock('../../queries/userQueries');
+afterEach(() => {
+	jest.clearAllMocks();
 })
 
 describe("Users controller", () => {
-    test("createUser should call correct query, return 201 and new user data", async () => {
-        console.log(userQueries.createUser)
+    test("if the request is good, it should call the correct db query, return 201 status and new user object", async () => {
         let req = mockRequest();
         req.body = {user: {handle:"Test Person", password:"NotARealPerson"}};
         const res = mockResponse();
@@ -21,5 +20,16 @@ describe("Users controller", () => {
         expect(res.status).toHaveBeenCalledWith(201)
         expect(userQueries.createUser).toHaveBeenCalledTimes(1)
         expect(res.json).toHaveBeenCalledWith({id: 1, handle: "Test Person"});
+    })
+    test("if there is no handle or password in the request, hould return 400 status and appropriate message", async () => {
+        let req = mockRequest();
+        req.body = {user: {handle:"", password:""}};
+        const res = mockResponse();
+    
+        await controller.createUser(req, res);
+    
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.send).toHaveBeenCalledWith('Missing required fields: handle or password');
+        expect(userQueries.createUser).not.toHaveBeenCalled()
     })
 })
