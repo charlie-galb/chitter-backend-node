5926038944d7fe12238317f1282a76b0458a9290
charlie-galb <galbraith.charlie@gmail.com>
2021-02-05 14:09:50 +0000

    Add test for unhappy path: user handle already exists

--- __tests__/integration/usersIntegration.test.js
+++ __tests__/integration/usersIntegration.test.js
@@ -14,34 +14,50 @@ afterAll( async () => {
 
 const app = createServer()
 
-test("POST /users", async () => {
-    const userData = {user: {handle:"kay", password:"mypassword"}}
-    await request(app)
-    .post("/users")
-    .send(userData)
-    .expect(201)
-    .then((response) => {
-        expect(response.body.user_id).toBe(1)
-        expect(response.body.handle).toBe("kay")
-     })
+describe("POST /users", () => {
+    test("when handle is not already in use it returns 201 status and new user as json", async () => {
+        const userData = {user: {handle:"kay", password:"mypassword"}}
+        await request(app)
+        .post("/users")
+        .send(userData)
+        .expect(201)
+        .then((response) => {
+            expect(response.body.user_id).toBe(1)
+            expect(response.body.handle).toBe("kay")
+         })
+    })
+    test("when handle is already in use it returns 404 status and error message", async () => {
+        const userData = {user: {handle:"kay", password:"differentpassword"}}
+        await request(app)
+        .post("/users")
+        .send(userData)
+        .then((response) => {
+            expect(response.status).toBe(404)
+            expect(response.text).toBe("Handle already taken")
+         })
+    })
 })
 
-test("GET /users", async () => {
-    await request(app)
-    .get("/users")
-    .expect(200)
-    .then((response) => {
-        expect(response.body.length).toBe(1)
-        expect(response.body[0].user_id).toBe(1)
-        expect(response.body[0].handle).toBe("kay")
-     })
+describe("GET /users", () => {
+    test("When the request is good it sends a 200 status and an array of user objects", async () => {
+        await request(app)
+        .get("/users")
+        .expect(200)
+        .then((response) => {
+            expect(response.body.length).toBe(1)
+            expect(response.body[0].user_id).toBe(1)
+            expect(response.body[0].handle).toBe("kay")
+         })
+    })
 })
 
-test("DELETE /users", async () => {
-    await request(app)
-    .delete("/users/1")
-    .expect(200)
-    .then((response) => {
-        expect(response.text).toBe("User successfully deleted")
-     })
+describe("DELETE /users", () => {
+    test("When the request is good it sends a 200 status and a confirmation message", async () => {
+        await request(app)
+        .delete("/users/1")
+        .expect(200)
+        .then((response) => {
+            expect(response.text).toBe("User successfully deleted")
+         })
+    })
 })
