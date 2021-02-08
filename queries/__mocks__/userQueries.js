00b53cd08ec6f7a86830e4d6a568ccf69497c1b4
charlie-galb <galbraith.charlie@gmail.com>
2021-02-08 17:02:05 +0000

    Change mocked  value to reflect knex readout

--- queries/__mocks__/userQueries.js
+++ queries/__mocks__/userQueries.js
@@ -1,6 +1,6 @@
 const getAllUsers = jest.fn().mockReturnValue([{rows:[{id: 1, handle: "Test Person"}]}]);
 
-const createUser = jest.fn().mockReturnValue({rows:[{id: 1, handle: "Test Person"}]});
+const createUser = jest.fn().mockReturnValue({id: 1, handle: "Test Person"});
 
 const deleteUser = jest.fn();
 
