22b00b16be4993f22539e5fea4ab93020433da76
charlie-galb <galbraith.charlie@gmail.com>
2021-02-08 17:00:18 +0000

    Remove unused import

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const pool = require("../db");
 const { getAllUsers, createUser, deleteUser } = require('../controllers/usersController')
 
 // get users
