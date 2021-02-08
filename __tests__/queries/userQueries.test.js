36c54afe0d12c12cabf93ef7b6b95e8ea137d076
charlie-galb <galbraith.charlie@gmail.com>
2021-02-08 15:13:55 +0000

    Reconfigure to use knex

--- __tests__/queries/userQueries.test.js
+++ __tests__/queries/userQueries.test.js
@@ -1,29 +1,29 @@
-const pool = require("../../db");
+const db = require("../../db/db");
 const { createUser, getAllUsers, deleteUser } = require("../../queries/userQueries");
-const userQueries = require('../../queries/userQueries')
+const knex = require('../../db/db');
 
 beforeAll( async () => {
-	return await pool.query(
-        "CREATE TABLE users(user_id SERIAL PRIMARY KEY, handle VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (100) NOT NULL, session_key VARCHAR(50), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
-        );
+	return knex.migrate.latest();
 })
 
 afterAll( async () => {
-	return await pool.query("DROP TABLE users;");
+	return knex.migrate
+      .rollback()
+      .then(() => knex.destroy());
 })
 
 describe('createUser', () => {
     test("when handle is not already in use it inserts user data into db and returns id and handle", async () => {
         const testUser = {handle: 'Test Person', password: 'password'}
-        result = await createUser(testUser.handle, testUser.password)
-        expect(result.rows[0]).toEqual({user_id: 1, handle: 'Test Person'})
+        result = await createUser(testUser)
+        expect(result).toEqual({id: 1, handle: 'Test Person'})
     })
     test("when handle is already in use it throws an error", async () => {
         const testUser = {handle: 'Test Person', password: 'password'}
         try {
-            await createUser(testUser.handle, testUser.password)
+            await createUser(testUser)
         } catch(error) {
-            expect(error.message).toEqual('duplicate key value violates unique constraint "users_handle_key"')
+            expect(error.message).toMatch(/duplicate key value violates unique/)
         }
     })
 })
@@ -31,13 +31,14 @@ describe('createUser', () => {
 describe('getAllUsers', () => {
     test("returns all users' handles and IDs as objects in an array", async () => {
         result = await getAllUsers()
-        expect(result.rows).toEqual([{user_id: 1, handle: "Test Person"}])
+        expect(result).toEqual([{id: 1, handle: "Test Person"}])
     })
 })
 
 describe('deleteUser', () => {
     test("Deletes specified user", async () => {
-        result = await deleteUser()
-        expect(result.rows).toEqual([])
+        await deleteUser(1)
+        result = await getAllUsers()
+        expect(result.length).toEqual(0)
     })
 })
--- queries/userQueries.js
+++ queries/userQueries.js
@@ -1,20 +1,28 @@
-const pool = require("../db");
+const db = require("../db/db");
 
 const getAllUsers = () => {
-    return pool.query("SELECT user_id, handle FROM users");
+    return db
+      .select("id", "handle")
+      .from("users")
+      .then(rows => rows);
 };
 
-const createUser = (handle, password) => {
-    return pool.query(
-        "INSERT INTO users (handle, password) VALUES($1, $2) RETURNING user_id, handle",
-        [handle, password]
-    )
+const createUser = (newUser) => {
+    return db
+      .insert(newUser)
+      .into("users")
+      .returning(["id", "handle"])
+      .then(rows => {
+        return rows[0];
+      });
 };
 
 const deleteUser = (id) => {
-    return pool.query("DELETE FROM users WHERE user_id = $1", [id])
+    return db("users")
+      .where({ id })
+      .delete();
 };
 
 module.exports = {
-    getAllUsers, createUser, deleteUser 
+    getAllUsers, createUser, deleteUser
 }
