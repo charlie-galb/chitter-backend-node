e7383044bd9cb26d7dda5e1f8e0a7ac9fa9831a6
charlie-galb <galbraith.charlie@gmail.com>
2021-02-09 17:00:46 +0000

    Add sessions router

--- createServer.js
+++ createServer.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const cors = require("cors");
 usersRouter = require('./routers/usersRouter');
+sessionsRouter = require('./routers/sessionsRouter'); 
 
 function createServer() {
 	const app = express()
 	app.use(cors());
     app.use(express.json());
-    app.use('/users', usersRouter);
+	app.use('/users', usersRouter);
+	app.use('/sessions', sessionsRouter);
 	return app
 }
 
