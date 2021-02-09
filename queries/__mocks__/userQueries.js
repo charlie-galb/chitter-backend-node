e16c01f07cab9ff6de6c225dae66b3ec4d621fed
charlie-galb <galbraith.charlie@gmail.com>
2021-02-09 17:04:03 +0000

    Add mock findUser query and password hash

--- queries/__mocks__/userQueries.js
+++ queries/__mocks__/userQueries.js
@@ -1,9 +1,15 @@
+const bcrypt = require('bcrypt');
+
+const hashed_password = bcrypt.hashSync("NotARealPerson", saltRounds = 10)
+
 const getAllUsers = jest.fn().mockReturnValue([{rows:[{id: 1, handle: "Test Person"}]}]);
 
 const createUser = jest.fn().mockReturnValue({id: 1, handle: "Test Person"});
 
 const deleteUser = jest.fn();
 
+const findUser = jest.fn().mockReturnValue({id: 1, handle: "Test Person", password: hashed_password});
+
 module.exports = {
-    getAllUsers, createUser, deleteUser 
+    getAllUsers, createUser, deleteUser, findUser 
 }
