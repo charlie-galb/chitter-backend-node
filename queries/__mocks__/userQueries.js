3b6250cf2f3d56f69bcc317e5efd5789352de528
charlie-galb <galbraith.charlie@gmail.com>
2021-02-10 15:37:05 +0000

    Add mock for storeAuthToken

--- queries/__mocks__/userQueries.js
+++ queries/__mocks__/userQueries.js
@@ -10,6 +10,8 @@ const deleteUser = jest.fn();
 
 const findUser = jest.fn().mockReturnValue({id: 1, handle: "Test Person", password: hashed_password});
 
+const storeAuthToken = jest.fn();
+
 module.exports = {
-    getAllUsers, createUser, deleteUser, findUser 
+    getAllUsers, createUser, deleteUser, findUser, storeAuthToken 
 }
