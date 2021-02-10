5d349f5ecdc27892619b0d0bf55d02dc04895002
charlie-galb <galbraith.charlie@gmail.com>
2021-02-10 15:36:37 +0000

    rename session_key column to auth_token

--- db/migrations/20210208135748_create_users_table.js
+++ db/migrations/20210208135748_create_users_table.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
         table.increments();
         table.string('handle').unique().notNullable();
         table.string('password').notNullable();
-        table.string('session_key');
+        table.string('auth_token');
         table.timestamp('created_at').defaultTo(knex.fn.now())
         table.timestamp('updated_at').defaultTo(knex.fn.now())
       })
