c626b08cbf65667066ff9b2032639946b0d36723
charlie-galb <galbraith.charlie@gmail.com>
2021-02-16 12:38:40 +0000

    Add migration for peeps table

---
+++ db/migrations/20210216120743_create_peeps_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('peeps', function(table) {
+      table.increments();
+      table.string('body').notNullable();
+      table.integer('user_id').notNullable().references('id').inTable('users');
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+  }
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('peeps');
+  }
