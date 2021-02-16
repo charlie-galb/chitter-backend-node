57e095c7a3a9686ed6ebd3bbccdc4e527ffb6311
charlie-galb <galbraith.charlie@gmail.com>
2021-02-16 12:38:56 +0000

    Add seeding for peeps table

---
+++ db/seeds/test/02_peeps.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('peeps').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('peeps').insert([
+        {id: 1, body: 'test peep 1', user_id: 1},
+        {id: 2, body: 'test peep 2', user_id: 2},
+        {id: 3, body: 'test peep 3', user_id: 3}
+      ]);
+    });
+};
