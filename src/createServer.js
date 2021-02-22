0cbee27e577dffd018becf5088bbd0f2866b4157
charlie-galb <galbraith.charlie@gmail.com>
2021-02-22 15:54:42 +0000

    Add empty get route for homepage

--- src/createServer.js
+++ src/createServer.js
@@ -8,6 +8,9 @@ function createServer() {
 	const app = express()
 	app.use(cors());
     app.use(express.json());
+	app.get('/', function (req, res) {
+		res.send('Hello World!')
+	  })
 	app.use('/users', usersRouter);
 	app.use('/sessions', sessionsRouter);
 	app.use('/peeps', peepsRouter);
