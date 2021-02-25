bd221235d2a8388b3f9b14f390ffed86005d7abb
charlie-galb <galbraith.charlie@gmail.com>
2021-02-25 13:04:50 +0000

    Remove explicit id from insert

--- db/seeds/test/02_peeps.js
+++ db/seeds/test/02_peeps.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('peeps').insert([
-        {id: 1, body: 'test peep 1', user_id: 1},
-        {id: 2, body: 'test peep 2', user_id: 2},
-        {id: 3, body: 'test peep 3', user_id: 3}
+        {body: 'test peep 1', user_id: 1},
+        {body: 'test peep 2', user_id: 2},
+        {body: 'test peep 3', user_id: 3}
       ]);
     });
 };
