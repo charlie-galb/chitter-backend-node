6ffbb8993f538ba42563e1c6aa4ef46b16d2b638
charlie-galb <galbraith.charlie@gmail.com>
2021-02-25 13:05:35 +0000

    Add mock function for createPeep

--- src/queries/__mocks__/peepQueries.js
+++ src/queries/__mocks__/peepQueries.js
@@ -1,5 +1,7 @@
 const getAllPeeps = jest.fn().mockReturnValue([{id: 1, body: "mock peep", user_id: 1}]);
-
+const createPeep = jest.fn()
+   
 module.exports = {
-    getAllPeeps 
+    getAllPeeps,
+    createPeep 
 }
