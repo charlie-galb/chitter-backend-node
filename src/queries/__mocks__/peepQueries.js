fd82eb3a0f40a1756a7d49a3f99b438fd3d4bab9
charlie-galb <galbraith.charlie@gmail.com>
2021-02-26 10:24:33 +0000

    Add mock for deletePeepById

--- src/queries/__mocks__/peepQueries.js
+++ src/queries/__mocks__/peepQueries.js
@@ -1,7 +1,9 @@
 const retrievePeeps = jest.fn().mockReturnValue([{id: 1, body: "mock peep", user_id: 1}]);
-const savePeep = jest.fn()
+const savePeep = jest.fn();
+const deletePeepById = jest.fn();
    
 module.exports = {
     retrievePeeps,
-    savePeep 
+    savePeep,
+    deletePeepById 
 }
