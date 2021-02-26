1e5b7f6a47fb7eea597817b699cee158cec34521
charlie-galb <galbraith.charlie@gmail.com>
2021-02-26 10:28:48 +0000

    Add tests for post and delete

--- __tests__/integration/peepsIntegration.test.js
+++ __tests__/integration/peepsIntegration.test.js
@@ -16,13 +16,11 @@ afterAll( async () => {
 
 const app = createServer()
 
-describe("Get /peeps", () => {
+const payload = {handle: 'mock handle'}
+const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {algorithm: "HS256", expiresIn: 60 * 60})
+
+describe("GET /peeps", () => {
     test("if request good, returns 200 status and 50 latest peeps", async () => {
-        const payload = {handle: 'mock handle'}
-        const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-            algorithm: "HS256",
-            expiresIn: 60 * 60
-        })
         await request(app)
         .get("/peeps")
         .set('authorization', token)
@@ -48,6 +46,35 @@ describe("Get /peeps", () => {
         .expect(401)
         .then((response) => {
             expect(response.body.message).toEqual("Unauthorized Access!")
+         })
+    })
+})
+
+describe("POST /peeps", () => {
+    test("if request is good, returns 201 status and newly created peep", async () => {
+        const peep = {peep: {user_id:"1", body:"Not a real peep"}}
+        await request(app)
+        .post("/peeps")
+        .set('authorization', token)
+        .send(peep)
+        .expect(201)
+        .then((response) => {
+            expect(response.body.user_id).toEqual(1)
+            expect(response.body.id).toEqual(4)
+            expect(response.body.body).toEqual("Not a real peep")
+         })
+    })
+})
+
+describe("DELETE /peeps", () => {
+    test("if request is good, returns 200 status and a confirmation message", async () => {
+        const peep = {peep: {user_id:"1", body:"Not a real peep"}}
+        await request(app)
+        .delete("/peeps/1")
+        .set('authorization', token)
+        .expect(200)
+        .then((response) => {
+            expect(response.text).toEqual("Peep successfully deleted")
          })
     })
 })
