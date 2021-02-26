503ffef93b24fb3a58362a798e714856241b2be9
charlie-galb <galbraith.charlie@gmail.com>
2021-02-26 11:37:59 +0000

    Add likes table migration

---
+++ db/migrations/20210226104211_create_likes_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('likes', function(table) {
+      table.increments();
+      table.unique(['user_id', 'peep_id']);
+      table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE');
+      table.integer('peep_id').notNullable().references('id').inTable('peeps').onDelete('CASCADE');
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+  }
+  
+exports.down = function(knex) {
+return knex.schema.dropTable('likes');
+}
+
