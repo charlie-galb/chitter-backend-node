04213a4c54fa9f17f062dab8614ad6bbc6b547b5
charlie-galb <galbraith.charlie@gmail.com>
2021-02-26 11:37:15 +0000

    Remove explicit ids from seeded users

--- db/seeds/test/01_users.js
+++ db/seeds/test/01_users.js
@@ -8,17 +8,14 @@ exports.seed = function(knex, Promise) {
     // Inserts seed entries
     return knex('users').insert([
       {
-        id: 1,
         handle: 'nigel',
         password: hash
       },
       {
-        id: 2,
         handle: 'nakaz',
         password: hash
       },
       {
-        id: 3,
         handle: 'jaywon',
         password: hash
       }
