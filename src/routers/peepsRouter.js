e496a60e675e2796cf2023abd52a381ccf13e367
charlie-galb <galbraith.charlie@gmail.com>
2021-02-26 11:59:35 +0000

    Add likes put route

--- src/routers/peepsRouter.js
+++ src/routers/peepsRouter.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { getAllPeeps, createPeep, deletePeep } = require('../controllers/peepsController')
+const { createLike } = require('../controllers/likesController')
 const verify =  require("../middleware/verify")
 
 router.get('/', verify, getAllPeeps);
 router.post('/', verify, createPeep);
 router.delete('/:id', verify, deletePeep);
+
+router.put('/:peep_id/likes/:user_id', verify, createLike);
 
 module.exports = router
