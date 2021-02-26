727d2cab93f09cb34e367a5235cec5b29e30f8a7
charlie-galb <galbraith.charlie@gmail.com>
2021-02-26 10:23:56 +0000

    Add post and delete routes

--- src/routers/peepsRouter.js
+++ src/routers/peepsRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAllPeeps } = require('../controllers/peepsController')
+const { getAllPeeps, createPeep, deletePeep } = require('../controllers/peepsController')
 const verify =  require("../middleware/verify")
 
 router.get('/', verify, getAllPeeps);
+router.post('/', verify, createPeep);
+router.delete('/:id', verify, deletePeep);
 
 module.exports = router
