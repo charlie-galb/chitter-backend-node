d7a7ce7ccb468dc32ecc02f8842ec6b9b39daf3f
charlie-galb <galbraith.charlie@gmail.com>
2021-02-26 10:28:10 +0000

    Update function names in accordance with source files

--- __tests__/queries/userQueries.test.js
+++ __tests__/queries/userQueries.test.js
@@ -1,8 +1,9 @@
-const { saveUser, retrieveUsers, deleteUserFromDb, findUser, storeAuthToken } = require("../../src/queries/userQueries");
+const { saveUser, retrieveUsers, deleteUserById, findUserByHandle, storeAuthToken } = require("../../src/queries/userQueries");
 const knex = require('../../db/db');
 
 beforeAll( async () => {
-	return knex.migrate.latest();
+	return knex.migrate.latest()
+        .then(() => { return knex.seed.run(); });
 })
 
 afterAll( async () => {
@@ -11,48 +12,50 @@ afterAll( async () => {
       .then(() => knex.destroy());
 })
 
-const testUser = {handle: 'Test Person', password: 'password'}
+describe('retrieveUsers', () => {
+    test("returns all users' handles and IDs as objects in an array", async () => {
+        result = await retrieveUsers()
+        expect(result.length).toEqual(3)
+        expect(result[0].handle).toEqual("nigel")
+    })
+})
 
 describe('saveUser', () => {
     test("when handle is not already in use it inserts user data into db and returns id and handle", async () => {
+        const testUser = {id: 4, handle: 'Mona', password: 'password'}
         result = await saveUser(testUser)
-        expect(result).toEqual({id: 1, handle: 'Test Person'})
+        expect(result).toEqual({id: 4, handle: 'Mona'})
     })
     test("when handle is already in use it throws an error", async () => {
+        const seededUser = {handle: 'nigel', password: 'password'}
         try {
-            await saveUser(testUser)
+            await saveUser(seededUser)
         } catch(error) {
             expect(error.message).toMatch(/duplicate key value violates unique/)
         }
     })
 })
 
-describe('retrieveUsers', () => {
-    test("returns all users' handles and IDs as objects in an array", async () => {
-        result = await retrieveUsers()
-        expect(result).toEqual([{id: 1, handle: "Test Person"}])
-    })
-})
-
-describe('findUser', () => {
+describe('findUserByHandle', () => {
     test("returns a single user that matches the handle and password provided", async () => {
-        result = await findUser(testUser)
-        expect(result).toEqual({id: 1, handle: "Test Person", password: "password"})
+        result = await findUserByHandle("nigel")
+        expect(result.id).toEqual(1)
+        expect(result.handle).toEqual("nigel")
     })
 })
 
 describe('storeAuthToken', () => {
     test("stores user's JWT refresh token", async () => {
         const authToken = "ajfk34tgjdvij56"
-        result = await storeAuthToken('Test Person', authToken)
-        expect(result).toEqual({"handle": "Test Person", "id": 1})
+        result = await storeAuthToken('nigel', authToken)
+        expect(result.handle).toEqual("nigel")
     })
 })
 
-describe('deleteUserFromDb', () => {
+describe('deleteUserById', () => {
     test("Deletes specified user", async () => {
-        await deleteUserFromDb(1)
+        await deleteUserById(1)
         result = await retrieveUsers()
-        expect(result.length).toEqual(0)
+        expect(result[0].handle).not.toEqual("nigel")
     })
 })
