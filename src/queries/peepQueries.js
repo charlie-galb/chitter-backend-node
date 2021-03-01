cea6e4d208497ecec349598d626949818d063838
charlie-galb <galbraith.charlie@gmail.com>
2021-03-01 17:32:41 +0000

    Reformat retirevePeeps query to return nested user object and nested array of likes

--- src/queries/peepQueries.js
+++ src/queries/peepQueries.js
@@ -1,9 +1,20 @@
 const db = require("../../db/db");
 
 const retrievePeeps = () => {
-    return db
-      .select("*")
-      .from("peeps")
+    return db("peeps")
+      .leftJoin('likes','peeps.id','likes.peep_id')
+      .leftJoin('users', 'users.id', 'peeps.user_id')
+      .select([
+        'peeps.id',
+        'peeps.user_id',
+        'peeps.body',
+        'peeps.created_at',
+        'peeps.updated_at',
+        db.raw(`COALESCE (json_strip_nulls(json_build_object('id', users.id, 'handle', users.handle))) as user`),
+        db.raw(`CASE WHEN likes.peep_id IS NOT NULL THEN ARRAY_AGG(json_build_object('id', likes.id, 'user_id', likes.user_id)) ELSE NULL END as likes`)
+      ])
+      .orderBy('peeps.id', 'desc')
+      .groupBy('peeps.id', 'peeps.body', 'users.id', 'users.handle', 'likes.peep_id')
       .then(rows => rows);
 };
 
